ccd978d336341ed28d2ecec6320cd2b1eb964122
Hieser21 <rarafat883@proton.me>
2023-12-20 09:51:56 +0000

    barebones update

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ app.post('/', function (req, res) {
 	console.log(req.body);
 	res.end();
 });
-
 app.listen(PORT, function (err) {
 	if (err) console.log(err);
 	console.log("Server listening on PORT", PORT);
